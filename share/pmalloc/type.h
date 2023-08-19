de07c09060b94c060b9d130ed70103c23a9711eb
Yamanekazuma <ka6ka911k0@outlook.kp>
2023-08-20 00:04:46 +0900

    MSVCとその他のコンパイラ間の差を緩和

--- share/pmalloc/type.h
+++ share/pmalloc/type.h
@@ -1,3 +1,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#ifdef _MSC_VER
+#define ATTRIBUTE_TLS __declspec(thread)
+#define alignas _Alignas
+#else
+#include <stdalign.h>
+#include <threads.h>
+#define ATTRIBUTE_TLS thread_local
+#endif
