86d6ee1ab3fe594e9070b5233981d36ff966875b
Yamanekazuma <ka6ka911k0@outlook.kp>
2023-08-20 00:07:42 +0900

    pfreeを実装

--- src/pfree.c
+++ src/pfree.c
@@ -1,5 +1,19 @@
 #include "pmalloc.h"
+#include "arena.h"
 
 void pfree(in void* ptr) {
-	/* Not implemented */
+	if (ptr == NULL) {
+		return;
+	}
+
+	_pmalloc_chunk* chunk = _pmalloc_ptr2chunk(ptr);
+	size_t binIdx = _pmalloc_chunk2binidx(chunk);
+	_pmalloc_state* arena = _pmalloc_chunk2arena(chunk);
+	size_t count = arena-> counts[binIdx];
+	if (binIdx == 0 || count >= _PM_FILL_COUNT) {
+		HeapFree(arena->heap, HEAP_NO_SERIALIZE, chunk);
+	}
+	else {
+		_pmalloc_push_bin(arena, binIdx, _pmalloc_chunk2ptr(chunk));
+	}
 }
