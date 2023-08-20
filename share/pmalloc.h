2092fffd087d5438b3071a647a68b099da75bd62
Yamanekazuma <ka6ka911k0@outlook.kp>
2023-08-21 00:37:31 +0900

    dllexport/dllimport指定を削除

--- share/pmalloc.h
+++ share/pmalloc.h
@@ -2,23 +2,17 @@
 
 #include <pmalloc/type.h>
 
-#ifdef PMALLOC_EXPORTS
-	#define PMALLOCAPI __declspec(dllexport)
-#else
-	#define PMALLOCAPI __declspec(dllimport)
-#endif
-
 #define in
 #define out
 
 
-PMALLOCAPI void* pmalloc(in size_t size);
-PMALLOCAPI void* pcalloc(in size_t size);
-PMALLOCAPI void* prealloc(in out void* ptr, in size_t size);
+void* pmalloc(in size_t size);
+void* pcalloc(in size_t size);
+void* prealloc(in out void* ptr, in size_t size);
 
-PMALLOCAPI void* pmalloc_aligned(in size_t size, in size_t alignment);
-PMALLOCAPI void* pcalloc_aligned(in size_t size, in size_t alignment);
-PMALLOCAPI void* prealloc_aligned(in out void* ptr, in size_t size, in size_t alignment);
+void* pmalloc_aligned(in size_t size, in size_t alignment);
+void* pcalloc_aligned(in size_t size, in size_t alignment);
+void* prealloc_aligned(in out void* ptr, in size_t size, in size_t alignment);
 
-PMALLOCAPI void pfree(in void* ptr);
-PMALLOCAPI void pfree_aligned(in void* ptr);
+void pfree(in void* ptr);
+void pfree_aligned(in void* ptr);
